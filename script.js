6400558c8c5b77b51d6fdf1ad740b61734b9a9cc
menacemanlabrat <denzg0025@gmail.com>
2025-06-15 12:34:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  document.getElementById("navMenu").classList.toggle("show");
-}
-
-function toggleTheme() {
-  const html = document.documentElement;
-  const currentTheme = html.getAttribute("data-theme");
-  html.setAttribute("data-theme", currentTheme === "light" ? "dark" : "light");
-}
